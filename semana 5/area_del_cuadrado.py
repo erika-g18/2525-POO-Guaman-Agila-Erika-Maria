6a7b1cadc5b3aa4225f2cfe1100c0f40b815c6db
erika-g18 <em.guamana@uea.edu.ec>
2025-06-28 23:18:35 -0500

    Add files via upload

---
+++ semana 5/area_del_cuadrado.py
@@ -0,0 +1,35 @@
+#programa para calcular el area del cuadrado,
+
+#definir dados (integer)
+lado_1 = 8
+lado_2 = lado_1
+
+#formula para calcular el area del cuadrado
+area_del_cuadrado = lado_1 * lado_2
+print("el area del cuadradoes:",area_del_cuadrado)
+print("========================================")
+
+#programa para cambiar kilimetros a metros
+
+#definir dados (float)
+
+kilo_metros = 12.5
+
+#formula para calcular kilimetros a metros
+
+metros = kilo_metros * 1000
+
+print("12.5km es igual a:",metros,"m")
+print("==============================")
+
+#programa para registrase en un curso de cocina
+#datos tipo string
+nombre = input("ingrese su nombre: ")
+apellido = input ("ingrese su apellido: ")
+cuidad= input ("ingrese la cuidad que reside:")
+
+#mostrar un mensaje que se a registrado correctamente
+print("hola",nombre, apellido, "de la ciudad:",cuidad)
+print("has sido registrada correctamente al curso de cocina")
+
+
